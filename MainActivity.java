84ed9f66464756dd75d2cba8fc0b6b66b391f1f5
kbansal11 <krtkbansal230@gmail.com>
2019-02-10 20:35:32 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.kartik.prEemploy.;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable(){
+
+            public void run(){
+                Intent homeintent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(homeintent);
+                finish();
+
+
+            }
+
+        },SPLASH_TIME_OUT);
+
+    }
+}
